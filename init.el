4c8a37d95ae3e4728f6e48d2934e5cf247437076
Nikolay <nikolay@blokhin.us>
2017-05-05 07:40:11 +0000

    Added new init.el

---
+++ init.el
@@ -0,0 +1,147 @@
+;;; init.el --- Emacs initialization file -*- lexical-binding: t -*-
+;; Copyright (c) 2011-2017, Nikolay Blokhin
+;; All rights reserved.
+
+;;; Commentary:
+;; Do not forget feed cats.
+
+;;; Code:
+
+;; Path to my libs and scripts
+(add-to-list 'load-path "~/.emacs.d/libs/")
+(add-to-list 'load-path "~/.emacs.d/scripts/")
+(add-to-list 'load-path "~/.emacs.d/elpa/")
+
+(require 'package)
+(require 'ido)
+(require 'python-mode)
+(require 'sr-speedbar)
+(require 'paren)
+
+;; add packages repository
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+
+;; turn on auto-complete
+(ac-config-default)
+
+(global-flycheck-mode)
+
+(ido-mode t)
+
+
+;; Standard Jedi.el setting
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+;; (setq jedi:environment-root '("/home/cloud/qtakecloud/env/"))
+
+;; Type:
+;;     M-x package-install RET jedi RET
+;;     M-x jedi:install-server RET
+;; Then open Python file.
+
+
+
+;; Truncate lines (turn off wrapping for long lines)
+(set-default 'truncate-lines t)
+
+
+
+(custom-set-variables
+ '(global-linum-mode t)
+ '(inhibit-startup-screen t)
+ '(require-final-newline t)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ )
+
+;; Hide Emacs menu sting
+(menu-bar-mode -1)
+(global-hl-line-mode +1)
+
+;; see matching pairs of parentheses and other characters
+(show-paren-mode 1)
+(set-face-background 'show-paren-match (face-background 'default))
+(set-face-foreground 'show-paren-match "#d700d7")
+    (set-face-attribute 'show-paren-match nil :weight 'extra-bold)
+
+
+;; Show the column and line numbers
+(setq line-number-mode t)
+(setq column-number-mode t)
+(setq linum-format "%4d ")
+
+;; For JavaScript
+(setq js-indent-level 4)
+(setq js-switch-indent-offset 4)
+(setq-default indent-tabs-mode nil)
+
+
+
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil)            ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't)                  ;; scroll window under mouse
+(setq scroll-step 1)                                ;; keyboard scroll one line at a time
+
+
+;;no extra whitespace after lines
+(add-hook 'before-save-hook 'whitespace-cleanup)
+
+
+
+;; ############################################################################
+;;                          Setup Shortcuts
+;; ############################################################################
+
+;; Show "buffer" with "cuts"
+(global-set-key "\M-y" 'browse-kill-ring)
+
+;; Move line or block up/down
+(global-set-key "\M-n" 'move-text-down)
+(global-set-key "\M-p" 'move-text-up)
+
+;; Toggle SrSpeebar
+(global-set-key (kbd "C-c s") 'sr-speedbar-toggle)
+
+;; ###########################################################################
+;;                        Color Theme Stuff
+;; ###########################################################################
+
+
+;; current color theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+(load-theme 'solarized t)
+
+
+;; Add syntax highlighting for operators ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+;; Currently support for []|&!.+=-/%*,()<>{}
+(font-lock-add-keywords
+ 'python-mode '(("\\(\\[\\|\\]\\|[|!\\.\\+\\=\\&]\\|-\\|\\/\\|\\:\\|\\%\\|\\*\\|,\\|(\\|)\\|>\\ |<\\|{\\|}\\)" 1 font-lock-operator-face )
+                ("\\(;\\)" 1 font-lock-end-statement )))
+
+(make-face 'font-lock-operator-face)
+(make-face 'font-lock-end-statement)
+(setq font-lock-operator-face 'font-lock-operator-face)
+(setq font-lock-end-statement 'font-lock-end-statement)
+;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+;; Setup my custom colors, but Emacs must be run through
+;; color preset "Solarized Dark" in iTerm2
+;; Color names got from this site:  https://jonasjacek.github.io/colors/
+(custom-set-faces
+ '(font-lock-function-name-face ((t (:foreground "Gold1"))))
+ '(font-lock-keyword-face ((t (:foreground "SteelBlue1"))))
+ '(font-lock-string-face ((t (:foreground "#00af5f"))))
+ '(font-lock-type-face ((t (:foreground "Orange1" :bold t))))
+ '(font-lock-builtin-face ((t (:foreground "SlateBlue3"))))
+ '(font-lock-variable-name-face ((t (:foreground "#949494"))))
+ '(font-lock-comment-face ((t (:foreground "#559fff"))))
+ '(font-lock-operator-face ((t (:foreground "#d7af00"))))
+ '(py-decorators-face ((t (:foreground "#ad7fa8" :bold t))))
+ )
+
+;;; init.el ends here
