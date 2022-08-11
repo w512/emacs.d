83a1575f60d527e128da03c189d27da0cc4ead23
Nick Blokhin <nick@blokhin.us>
2022-08-11 20:21:55 +0200

    In progress

--- init.el
+++ init.el
@@ -8,6 +8,10 @@
 ;;; Code:
 
 
+;; Move customization variables to a separate file and load it
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(load custom-file 'noerror 'nomessage)
+
 ;;
 ;; General stuff for packages ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;;
@@ -31,9 +35,9 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; list the packages you want
+;; list the packages you are used
 (setq package-list
-      '(web-mode tangotango-theme doom-themes))
+      '(web-mode tangotango-theme doom-themes all-the-icons))
 
 ;; install the missing packages
 (dolist (package package-list)
@@ -41,24 +45,10 @@
 	(package-install package)))
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(doom-themes web-mode tangotango-theme nord-theme use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-
 ;;
 ;; web-mode ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;;
+
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
@@ -82,12 +72,19 @@
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 
+(global-display-fill-column-indicator-mode t)            ;; Show right border
+(setq-default display-fill-column-indicator-column 119)  ;; Border position
+(set-face-foreground 'fill-column-indicator "#004400")   ;; Border color
+
 ;;
 ;; Cursor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;;
 (blink-cursor-mode 2)
 (setq-default cursor-type 'bar)
 (set-cursor-color "#339933") 
+
+(global-hl-line-mode t)                      ;; Highlight line with cursor
+(set-face-background 'hl-line "#3e4446")     ;; Highlight color of line with cursor
 
 
 ;;
@@ -106,7 +103,7 @@
 
 (set-face-attribute 'default nil
 					:family "JetBrains Mono"
-					:height 180)              ;; Setup font
+					:height 160)              ;; Setup font
 
 (setq line-spacing 0.3)					      ;;  space to put between lines
 
@@ -119,49 +116,41 @@
 (setq-default truncate-lines t)               ;; Don't wrap lines
 (fset 'yes-or-no-p 'y-or-n-p)                 ;; y/n for  answering yes/no questions
 (delete-selection-mode t)                     ;; Delete the selection with a keypress
-(global-auto-revert-mode t)                   ;; Update buffers when files are changed externally
 (electric-pair-mode t)                        ;; Autoclose {},[],()
-
-(global-display-fill-column-indicator-mode t)            ;; Show right border
-(setq-default display-fill-column-indicator-column 119)  ;; Border position
-(set-face-foreground 'fill-column-indicator "#004400")   ;; Border color
-
-(global-hl-line-mode t)                       ;; Highlight line with cursor
-(set-face-background 'hl-line "#3e4446")      ;; Highlight color of line with cursor
-
+(global-auto-revert-mode t)                   ;; Update buffers when files are changed externally
+(setq global-auto-revert-non-file-buffers t)  ;; Update Dired and other buffers on change
 
 (if (display-graphic-p)
     (progn
-      (scroll-bar-mode -1)          ; Disable visible scrollbar
-      (tool-bar-mode -1)            ; Disable the toolbar
-      (menu-bar-mode -1)))          ; Disable the menu bar
+      (scroll-bar-mode -1)     ;; Disable visible scrollbar
+      (tool-bar-mode -1)       ;; Disable the toolbar
+      (menu-bar-mode -1)))     ;; Disable the menu bar
 
 
 (require 'ido)
 (ido-mode t)
 (setq ido-enable-flex-matching t)
 
-
 ;; (load-theme 'tangotango t)
 ;; (load-theme 'nord t)
+
+
+(use-package all-the-icons
+  :if (display-graphic-p))
 
 (use-package doom-themes
   :ensure t
   :config
   ;; Global settings (defaults)
-  (setq doom-themes-enable-bold nil    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-spacegrey t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
+  (setq doom-themes-enable-bold nil    ;; if nil, bold is universally disabled
+        doom-themes-enable-italic t)   ;; if nil, italics is universally disabled
+  (load-theme 'doom-solarized-dark t)  ;; select current theme (another good theme is "doom-spacegrey")
+  (doom-themes-visual-bell-config)     ;; Enable flashing mode-line on errors
+  (doom-themes-neotree-config)         ;; Enable custom neotree theme (all-the-icons must be installed!)
   ;; or for treemacs users
   (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
   (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+  (doom-themes-org-config))            ;; Corrects (and improves) org-mode's native fontification.
 
 ;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -194,6 +183,48 @@
 (global-set-key [(control super up)]  'move-line-up)
 (global-set-key [(control super down)]  'move-line-down)
 (global-set-key [(super shift d)] 'duplicate-line)
+
+
+;; For Wind Move ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+(global-set-key (kbd "s-<left>")  'windmove-left)
+(global-set-key (kbd "s-<right>") 'windmove-right)
+(global-set-key (kbd "s-<up>")    'windmove-up)
+(global-set-key (kbd "s-<down>")  'windmove-down)
+;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+;;
+;; Colors customisation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+;;
+
+(set-face-foreground 'web-mode-keyword-face "#bbbf2a")
+(set-face-foreground 'web-mode-html-tag-bracket-face "#879393")
+(set-face-foreground 'web-mode-html-attr-value-face "#009933")
+(set-face-foreground 'web-mode-javascript-string-face "#009933")
+
+
+(defvar font-lock-punctuation-face		
+  'font-lock-my-face
+  "Face name to use for punctuation.")
+
+(defface font-lock-punctuation-face
+  '((t (:foreground "#ffff00")))
+  "Font Lock mode face used to highlight punctuation."
+  :group 'font-lock-faces)
+
+(mapc (lambda (mode)
+        (font-lock-add-keywords
+         mode
+         '(
+           ("\\({\\|}\\|[\\|]\\|(\\|)\\)" 0 'font-lock-punctuation-face prepend)
+           ("\\(:\\|=\\|;\\|\\+\\| - \\|\\-=\\|\\+=\\|>=\\|<=\\|!=\\|!==\\|=>\\)" 0 'font-lock-punctuation-face prepend)
+           ;("TODO:" 0 'font-lock-punctuation-face prepend)
+           ;("{%.*?block.*?%}" 0 'sm/django-block-face prepend)
+           ;<more lines here...>
+           )'end))
+      '(web-mode))
 
 
 
